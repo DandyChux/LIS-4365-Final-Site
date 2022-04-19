7d637a43b4c5c5adcae42fd48f6c29032dcc07ee
Chukwuma Okoroji <dandychux@gmail.com>
2022-04-19 11:48:12 -0400

    added db connection

--- connection.php
+++ connection.php
@@ -1,9 +1,15 @@
 <?php
 
-    $con = mysqli_connect();
+    $host = "sql202.epizy.com";
+    $username = "epiz_30833591";
+    $password = "363s16BydSiwdL";
+    $dbname = "epiz_30833591_Trinity_Cafe_assignment"
+
+    // Creating databse connection
+    $con = mysqli_connect($host, $username, $password, $dbname);
 
     if(!$con) {
-        die("Connection Error");
+        die("Connection Error" . mysqli_connect_error());
     }
 
 ?>
